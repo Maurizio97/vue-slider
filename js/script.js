a7443feac7a793e94e2008aab307dfc017d1aa06
Maurizio <proiettomaurizio97@gmail.com>
2021-11-11 15:26:57 +0100

    es complete

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 
-const items = [
+/* const items = [
     'img/01.jpg',
     'img/02.jpg',
     'img/03.jpg',
@@ -21,110 +21,59 @@ const text = [
     'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
     'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
     'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
-]
+] */
 
-
-// elementi di pagina utili 
-const itemsCont = document.querySelector(".container-items");
-const thumdsCont = document.querySelector(".container-thumbs");
-/* const next = document.querySelector(".next");
-const previous = document.querySelector(".previous") */
-
-let item = "";
-let thumbs = "";
-
-for ( let i = 0; i < items.length; i++) {
-    item += `
-    <div class="item">
-        <img src="${items[i]}" alt="${title[i]}">
-    
-        <div class="text">
-            <h2>
-                ${title[i]}
-            </h2>
-            <p>
-                ${text[i]}
-            </p>
-        </div>
-    </div> 
-    `;
-
-    thumbs += `
-    <div class="thumb">
-        <img src="${items[i]}" alt="${title[i]}">
-    </div>
-    `;
-}
-
-itemsCont.innerHTML += item;
-thumdsCont.innerHTML += thumbs; 
-
-
-let clickControl = 0
-
-let thumbClick = document.getElementsByClassName("thumb")[clickControl];
-thumbClick.classList.add('active');
-let itemClick = document.getElementsByClassName("item")[clickControl];
-itemClick.classList.add('active');
-
-
-const next = document.querySelector(".next");
-const previous = document.querySelector(".previous");
-console.log(next);
-console.log(previous);
-//catturo il click per il next
-next.addEventListener("click",
-
-    function(){
-        clickControl++
-        //se la classe active è presente la elimino 
-            document.querySelector(".item.active").classList.remove('active');
-            document.querySelector(".thumb.active").classList.remove('active');
-        // controllo sul click. arrivato all'ultima immagine torna indietro
-        if (clickControl == (items.length)){
-            clickControl = 0
-        }
-
-        //ad ogni click aggiungo la classe all'elemento successivo nell'array
-        let itemClick = document.getElementsByClassName("item")[clickControl]
-        itemClick.classList.add('active');
-
-        //thumbClick.classList.remove('active');
-
-        //ad ogni click aggiungo la classe all'elemento successivo nell'array
-        let thumbClick = document.getElementsByClassName("thumb")[clickControl]
-        thumbClick.classList.add('active');
-        console.log(clickControl);
-    }
-
-);
-
-//catturo il click per il previous
-
-previous.addEventListener("click",
-
-    function(){
-        clickControl--
-        //se la classe active è presente la elimino 
-            document.querySelector(".item.active").classList.remove('active');
-            document.querySelector(".thumb.active").classList.remove('active');
+var carousel = new Vue({
+    el: "#carousel",
+    data: {
+        counter : 0,
+        slides: [
+            {
+                image: 'img/01.jpg',
+                title: 'Svezia',
+                text:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.'
+            },
+            {
+                image: 'img/02.jpg',
+                title: 'Svizzera',
+                text:'Lorem ipsum.'
+            },
+            {
+                image: 'img/03.jpg',
+                title: 'Gran Bretagna',
+                text:'Lorem ipsum, dolor sit amet consectetur adipisicing elit.'
+            },
+            {
+                image: 'img/04.jpg',
+                title: 'Germania',
+                text:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.'
+            },
+            {
+                image: 'img/05.jpg',
+                title: 'Paradise',
+                text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.'
+            }
+        ]
+    },
+    methods: {
+        prevSlide(){
+            if(this.counter === 0){
+                this.counter = this.slides.length - 1;
+            }else {
+                this.counter --;
+            }
+        },
+        nextSlide(){
+            if(this.counter === this.slides.length - 1){
+                this.counter = 0;
+            }else {
+                this.counter ++;
+            }
+        },
+    },
+    autoSlide(){
+        setInterval(() => {
             
-        // controllo sul click. arrivato all'ultima immagine torna indietro
-        if (clickControl < 0){
-            clickControl = (items.length - 1)
-        }
-
-        //ad ogni click aggiungo la classe all'elemento successivo nell'array
-        let itemClick = document.getElementsByClassName("item")[clickControl]
-        itemClick.classList.add('active');
-
-        //thumbClick.classList.remove('active');
-
-        //ad ogni click aggiungo la classe all'elemento successivo nell'array
-        let thumbClick = document.getElementsByClassName("thumb")[clickControl]
-        thumbClick.classList.add('active');
-        console.log(clickControl);
+        }, interval);
     }
-    
-);
-console.log(clickControl);
+});
